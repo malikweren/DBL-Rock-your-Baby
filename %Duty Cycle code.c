8727d850a115e2b642b4bf1c7fad94231c81044a
malikweren <89612215+malikweren@users.noreply.github.com>
2022-12-09 12:32:59 +0100

    Update %Duty Cycle code.c

--- %Duty Cycle code.c
+++ %Duty Cycle code.c
@@ -10,18 +10,19 @@ int dutyCycle = 0;
 
 void setup(){
   M5.begin(); //Init M5Core. Initialize M5Core
-  ledcSetup(Amotor, freq, resolution);
-  ledcSetup(Fmotor, freq, resolution);
-  ledcAttachPin(AmotorPin, Amotor);
-  ledcAttachPin(FmotorPin, Fmotor);
+  ledcSetup(Amotor, freq, resolution);`   //Setup the output channel for the Amplitude motor
+  ledcSetup(Fmotor, freq, resolution);    //setup the output channel for the Frequency motor
+  ledcAttachPin(AmotorPin, Amotor);   //connect the Amotor channel to the Amotor pin
+  ledcAttachPin(FmotorPin, Fmotor);   //connect the Fmotor channel to the Fmotor pin
 }
 
-void setDutyCycle(int dutyCycle, int channel) {
-  int analogValue = dutyCycle * 255 / 100;
-  ledcWrite(channel, analogValue);
+void setDutyCycle(int dutyCycle, int channel) {   //(dutyCycle percentage, Amotor/Fmotor)
+  dutyCycle = 100 - dutyCycle;   //invert the signal, because it gets inverted by the motor circuit
+  int analogValue = dutyCycle * 255 / 100;    //convert the dutyCycle to an analog value
+  ledcWrite(channel, analogValue);    //set the PWM output on the given pin
 }
 
-void loop() {
+void loop() {   //test program for the motors
   for(dutyCycle = 10; dutyCycle <= 90; dutyCycle += 20) {
     setDutyCycle(dutyCycle, Amotor);
     setDutyCycle(100-dutyCycle, Fmotor);
