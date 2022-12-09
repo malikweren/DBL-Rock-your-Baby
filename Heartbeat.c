6f48ff8e558554aeb4d81a58454b5906cfbf3f58
malikweren <89612215+malikweren@users.noreply.github.com>
2022-12-09 12:12:09 +0100

    Add files via upload

--- Heartbeat.c
+++ Heartbeat.c
@@ -1,44 +1,53 @@
 #include <M5Stack.h>
 
-const int ledPin = 26;
-const int sensorPin = 35;
+ 
+
+const int sensorPin = 36;       // define which pin is the input
 int tmpHigh = 0;
 int tmpLow = 0;
-int timr = 0;
-int period = 0;
-int heartRate = 0;
+float timr = 0;
+float period = 0;
+float heartRate = 0;              //This is the value of the heartrate which can be used in the algorithm code.
+float tempheartRate = 0;
+float triggervalue1 = 100;      // below this value the signal is considered to be voltage minimum (including error margin)
+float triggervalue2 = 4000;     // above this value the signal is considered to be voltage peak (including error margin)
+float analogInput = 50;
+
+ 
 
 void setup() {
   M5.begin();
-  pinMode (ledPin, OUTPUT);
-
 }
-
- 
 
 void loop() {
+analogInput = analogRead(sensorPin);          // read the input and store it in variable analogInput
 
-int analogInput = analogRead(sensorPin);
+if(analogInput > triggervalue2 && tmpHigh == 0){
 
-if(analogInput > 800 && tmpHigh == 0){          
-  timr = millis();
+  timr = 0;
   tmpHigh = 1;
+
 }
 
-if(analogInput < 800 && tmpHigh == 1){
+delay(2);                     //these two lines are the timer
+timr = timr + 2;
+
+if(analogInput < triggervalue1 && tmpHigh == 1){
+
   tmpLow = 1;
+
 }
 
- 
-
-if(analogInput > 800 && tmpLow == 1){
-  int temp2 = millis();
-  period = temp2 - timr;
-  heartRate = 60/(period/1000);
+if(analogInput > triggervalue2 && tmpLow == 1){
+  period = timr;
+  tempheartRate = 60/(period/1000);
   tmpLow = 0;
   tmpHigh = 0;
-  M5.Lcd.print(heartRate);
 
+
+  if (heartRate > 55 && heartRate < 240){       //remove outliers
+    heartRate = tempheartRate;
+  }
 }
 
 }
